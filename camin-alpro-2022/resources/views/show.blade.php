a8ae96f48beea1995f1a8c50b6a9e348ace9e6db
Nopalev <faadhilah56@gmail.com>
2022-03-12 20:55:02 +0700

    finishing touch

--- camin-alpro-2022/resources/views/show.blade.php
+++ camin-alpro-2022/resources/views/show.blade.php
@@ -4,9 +4,9 @@
 <section>
     <div class="container-xl">
             <h1 class="text-center">Daftar Camin Alpro 2022</h1>
-            <div class="row mt-4" style="">
+            <div class="row g-0" style="">
                 <div class="card col-3 mx-auto">
-                    <img src={{ $camin["image"] }} class="card-img-top" alt="...">
+                    <img src={{ $camin["image"] }} class="img-fluid rounded-start" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">
                             {{ $camin["nama"] }}
@@ -16,6 +16,7 @@
                         <p class="card-text">Angkatan : {{ $camin["angkatan"] }}</p>
                     </div>
                 </div>
+                <a class="btn btn-outline-dark btn-sm" href="/camin">Back</a>
             </div>
         </div>
     </section>
